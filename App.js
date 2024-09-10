4c6a59c81360b1193778f243d9f4f61f91d97617
Santi-rX <santiagorm025@gmail.com>
2024-09-09 22:31:56 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import StackScreen from './screens/StackScreen';
+import { useNavigation } from '@react-navigation/native';
+
+export default function App() {
+  return (
+      <StackScreen />
+  );
+}
